99cf36d59e294dbec4117d837e0bfeec660a06ba
clickonrefresh <8089328-clickonrefresh@users.noreply.gitlab.com>
2022-05-28 02:57:49 +0000

    initial

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/ZgCfsWXCm1E9Nqf2/scene.splinecode" />
+  );
+}
